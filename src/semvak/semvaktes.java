a56761a86979534e7ff23787f6e78c84da0c3fa2
SBC4 <SBC4@SBC13>
2015-04-30 19:13:37 +0700

    semvak

---
+++ src/semvak/semvaktes.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semvak;
+
+/**
+ *
+ * @author SBC4
+ */
+public class semvaktes {
+    
+}
